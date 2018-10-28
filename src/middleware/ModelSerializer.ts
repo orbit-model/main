6ff974a935bf3303e9c50cb47d08a5bdff7e192a
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2018-10-28 16:55:42 +0100

    some model serializer implementation

--- src/middleware/ModelSerializer.ts
+++ src/middleware/ModelSerializer.ts
@@ -42,7 +42,7 @@ export default class ModelSerializer implements ModelSerializerContract<HiddenOr
       if (attributes.hasOwnProperty(name)) {
         let attributeInfo: AttributeInfo = findAttributeInfoByName(reflection, name);
 
-        let beforeSetAttr = {model, attributes, setter, name, attributeInfo };
+        let beforeSetAttr = { model, attributes, setter, name, attributeInfo };
         registry.runHook(ServiceType.ModelSerializer, "beforeSetAttribute", beforeSetAttr);
         name = beforeSetAttr.name;
         attributeInfo = beforeSetAttr.attributeInfo;
@@ -58,18 +58,24 @@ export default class ModelSerializer implements ModelSerializerContract<HiddenOr
 
 
   getHiddenOrbit(model: HiddenOrbitProp): HiddenOrbit {
-    return undefined;
+    return model.__orbit;
   }
 
   setHiddenOrbit(model: HiddenOrbitProp, value: HiddenOrbit): void {
+    model.__orbit = value;
   }
 
 
   getOrbitReflection(klass: { new(): any }): OrbitReflection {
-    return undefined;
+    if (typeof klass["__orbitReflection"] === "object") {
+      return klass["__orbitReflection"];
+    } else {
+      throw new Error('Type-Error: given class is not a model ("__orbitReflection" is not a static property)');
+    }
   }
 
   setOrbitReflection(klass: { new(): any }, reflection: OrbitReflection): void {
+    klass["__orbitReflection"] = reflection;
   }
 
 
