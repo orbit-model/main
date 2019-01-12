f3ae65cdc4e6d8e44711c3351bfdd67bb77f3dd2
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2019-01-12 17:42:38 +0100

    export some stuff

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,11 @@
 import 'reflect-metadata';
 import Planet from './Planet';
+import { MiddlewareRegistry, DefaultMiddlewareRegistry, MiddlewareAdapter, DefaultMiddlewareAdapter } from "@orbit-model/main";
+
+
+const middlewareRegistry: MiddlewareRegistry = new DefaultMiddlewareRegistry();
+
+let middlewareAdapter: MiddlewareAdapter = new DefaultMiddlewareAdapter();
 
 console.log('test');
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export {default as attr} from './attr';
+export { default as attr } from './attr';
+
+export { default as MiddelwareAdapter } from './contracts/Adapter';
+export { default as Injectable } from './contracts/Injectable';
+
+export { default as MiddlewareRegistry } from './middleware/MiddlewareRegistry';
+
+export { default as DefaultMiddleWareRegistry } from './middleware/impl/MiddlewareRegistry';
+export { default as DefaultMiddelwareAdapter } from './middleware/impl/Adapter';
