54b26807444ad7f952d931cb5a69e009e7980bff
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2019-06-06 07:46:57 +0200

    some tests

---
+++ @orbit-model/di/tests/MigratableContainer.spec.ts
@@ -0,0 +1,33 @@
+import { assert, expect } from 'chai';
+import DefaultContainer from "../src/impl/DefaultContainer";
+import MigratableContainer from "../src/MigratableContainer";
+
+
+describe('di.Container', function () {
+  class Example {
+  }
+
+  let container: MigratableContainer;
+
+  beforeEach(function () {
+    container = new DefaultContainer()
+  });
+
+  describe('#registerInstantiatedSingleton', function () {
+    it('takes a singelton', function () {
+      expect(() => container.getNames('test')).to.throw(/namespace/i, "init state: empty");
+      let instance = new Example();
+      container.registerInstantiatedSingleton('test', 'example', Example, instance);
+      assert.include(container.getNames('test'), 'example');
+    });
+  });
+
+  describe('#registerObject', function () {
+    it('works', function(){
+      expect(() => container.getNames('test')).to.throw(/namespace/i, "init state: empty");
+      let inst = {};
+      container.registerObject('test', 'example', inst);
+      assert.strictEqual(container.get('test', 'example'), inst);
+    });
+  });
+});
