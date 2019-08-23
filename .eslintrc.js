8ad9c693691fbdf8557eeaf87acaa02eb084fe20
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2019-08-23 11:19:02 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "plugins": ["@typescript-eslint"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "project": [
+      "./tsconfig.json",
+    ],
+    //"tsconfigRootDir": "../../",
+  },
+  "rules": {
+    "@typescript-eslint/generic-type-naming": ["error", "^[A-Z]+$"],
+    "@typescript-eslint/no-require-imports": "error",
+    "@typescript-eslint/no-parameter-properties": "error"
+  }
+};
